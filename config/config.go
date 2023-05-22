ddc81ba0d97c932931b948c4b04fcb11454f9c28
Franklin Peñaranda <coffemanfp@gmail.com>
2023-05-22 18:53:00 -0500

    Added config model and folder documentation

---
+++ config/config.go
@@ -0,0 +1,28 @@
+package config
+
+// Config is a interface to get the config of a given implementation.
+type Config interface {
+	// Get will get all the ConfigInfo available in the implementation
+	Get() ConfigInfo
+}
+
+// ConfigInfo is the common	 structure to contain all the config fields.
+type ConfigInfo struct {
+	Server               server               `yaml:"server"`
+	PostgreSQLProperties postgreSQLProperties `yaml:"psql"`
+}
+
+type server struct {
+	Port           int      `yaml:"port"`
+	Host           string   `yaml:"host"`
+	AllowedOrigins []string `yaml:"allowed_origins"`
+	SecretKey      string   `yaml:"secret_key"`
+}
+
+type postgreSQLProperties struct {
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+	Name     string `yaml:"name"`
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+}
---
+++ config/doc.go
@@ -0,0 +1,4 @@
+// Package config handles all the config implementations like env and file config.
+// The supported config types are: yaml files and environment vars.
+
+package config
