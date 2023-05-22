e090939f01d93a381a8946a307d1c7fb78345bce
Franklin Peñaranda <coffemanfp@gmail.com>
2023-05-22 18:59:27 -0500

    Updating the main process

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/coffemanfp/todo/config"
+	"github.com/coffemanfp/todo/database"
+	"github.com/coffemanfp/todo/database/psql"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	_, err := config.NewEnvManagerConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// continue the start process
+}
+
+func setUpDatabase(conf config.ConfigInfo) (db database.Database, err error) {
+	db.Conn = psql.NewPostgreSQLConnector(
+		conf.PostgreSQLProperties.User,
+		conf.PostgreSQLProperties.Password,
+		conf.PostgreSQLProperties.Name,
+		conf.PostgreSQLProperties.Host,
+		conf.PostgreSQLProperties.Port,
+	)
+
+	err = db.Conn.Connect()
+	if err != nil {
+		return
+	}
+
+	accountRepo, err := psql.NewAccountRepository(db.Conn.(*psql.PostgreSQLConnector))
+	if err != nil {
+		return
+	}
+
+	db.Repositories = map[database.RepositoryID]interface{}{
+		database.ACCOUNT_REPOSITORY: accountRepo,
+	}
+	return
 }
