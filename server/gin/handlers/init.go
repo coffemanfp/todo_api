5a305d795f3c4502d58fc207b2fb94fba5eda37e
Franklin Peñaranda <coffemanfp@gmail.com>
2023-05-25 00:57:07 -0500

    Removing handlers map

--- server/gin/handlers/init.go
+++ server/gin/handlers/init.go
@@ -7,9 +7,6 @@ import (
 
 var db database.Repositories
 var conf config.ConfigInfo
-var handlers = map[string]Handler{
-	"login": Login{},
-}
 
 func Init(newDb database.Repositories, newConf config.ConfigInfo) {
 	db = newDb
