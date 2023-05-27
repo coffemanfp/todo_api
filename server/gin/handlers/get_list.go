3093c3e9bd53fcba91483980447b8d59bf05de25
Franklin Peñaranda <coffemanfp@gmail.com>
2023-05-26 21:43:02 -0500

    Adding the GetList handler

---
+++ server/gin/handlers/get_list.go
@@ -0,0 +1,44 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/coffemanfp/todo/database"
+	"github.com/coffemanfp/todo/list"
+	"github.com/gin-gonic/gin"
+)
+
+type GetList struct{}
+
+func (gl GetList) Do(c *gin.Context) {
+	repo, ok := getListRepository(c)
+	if !ok {
+		return
+	}
+
+	id, ok := gl.readListID(c)
+	if !ok {
+		return
+	}
+
+	l, ok := gl.getListFromDB(c, id, repo)
+	if !ok {
+		return
+	}
+
+	c.JSON(http.StatusOK, l)
+}
+
+func (gl GetList) readListID(c *gin.Context) (id int, ok bool) {
+	return readIntParam(c, "id")
+}
+
+func (gl GetList) getListFromDB(c *gin.Context, id int, repo database.ListRepository) (l list.List, ok bool) {
+	l, err := repo.GetList(id)
+	if err != nil {
+		handleError(c, err)
+		return
+	}
+	ok = true
+	return
+}
