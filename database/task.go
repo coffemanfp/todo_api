f24963a444109ec1e91c9473501db42f8e6a3212
Franklin Peñaranda <coffemanfp@gmail.com>
2023-05-26 21:37:51 -0500

    Adding a TaskRepository interface for the database

---
+++ database/task.go
@@ -0,0 +1,13 @@
+package database
+
+import "github.com/coffemanfp/todo/task"
+
+const TASK_REPOSITORY RepositoryID = "TASK"
+
+type TaskRepository interface {
+	CreateTask(task task.Task) (id int, err error)
+	UpdateTask(task task.Task) (err error)
+	DeleteTask(id int) (err error)
+	GetSomeTasks(page, createdBy int) (ts []*task.Task, err error)
+	GetTask(id int) (task task.Task, err error)
+}
