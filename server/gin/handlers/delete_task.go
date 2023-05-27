09c2e089cab41000f07ca232eb0da8546006c595
Franklin Peñaranda <coffemanfp@gmail.com>
2023-05-26 21:44:57 -0500

    Adding the DeleteTask handler

---
+++ server/gin/handlers/delete_task.go
@@ -0,0 +1,43 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/coffemanfp/todo/database"
+	"github.com/gin-gonic/gin"
+)
+
+type DeleteTask struct{}
+
+func (dt DeleteTask) Do(c *gin.Context) {
+	id, ok := dt.readTaskID(c)
+	if !ok {
+		return
+	}
+
+	repo, ok := getTaskRepository(c)
+	if !ok {
+		return
+	}
+
+	ok = dt.deleteTaskInDB(c, repo, id)
+	if !ok {
+		return
+	}
+
+	c.Status(http.StatusOK)
+}
+
+func (dt DeleteTask) readTaskID(c *gin.Context) (id int, ok bool) {
+	return readIntParam(c, "id")
+}
+
+func (dt DeleteTask) deleteTaskInDB(c *gin.Context, repo database.TaskRepository, id int) (ok bool) {
+	err := repo.DeleteTask(id)
+	if err != nil {
+		handleError(c, err)
+		return
+	}
+	ok = true
+	return
+}
