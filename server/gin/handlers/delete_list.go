67653ef519d060e030c3b12029a647e7c68d72a1
Franklin Peñaranda <coffemanfp@gmail.com>
2023-05-26 21:43:21 -0500

    Adding the DeleteList handler

---
+++ server/gin/handlers/delete_list.go
@@ -0,0 +1,43 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/coffemanfp/todo/database"
+	"github.com/gin-gonic/gin"
+)
+
+type DeleteList struct{}
+
+func (dl DeleteList) Do(c *gin.Context) {
+	id, ok := dl.readListId(c)
+	if !ok {
+		return
+	}
+
+	repo, ok := getListRepository(c)
+	if !ok {
+		return
+	}
+
+	ok = dl.deleteListFromDB(c, repo, id)
+	if !ok {
+		return
+	}
+
+	c.Status(http.StatusOK)
+}
+
+func (dl DeleteList) readListId(c *gin.Context) (id int, ok bool) {
+	return readIntParam(c, "id")
+}
+
+func (dl DeleteList) deleteListFromDB(c *gin.Context, repo database.ListRepository, id int) (ok bool) {
+	err := repo.DeleteList(id)
+	if err != nil {
+		handleError(c, err)
+		return
+	}
+	ok = true
+	return
+}
